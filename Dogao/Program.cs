260ef3d83464ad4bee06fa9ce39e5b38c6186906
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 15:02:57 -0300

    Add project files.

---
+++ Dogao/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dogao
+{
+    class Program
+    {
+        /// <summary>
+        /// O programa "Dogao" foi criado baseado na seguinte tabela
+        /// 
+        /// codigo  especifiação        preço
+        /// 1       Cachorro Quente     R$4.00
+        /// 2       X=Salada            R$4.50
+        /// 3       X=Bacon             R$5.00
+        /// 4       Torrada simples     R$2.00
+        /// 5       Refrigerante        R$1.50
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o código do item e sua quantidade separados por espaços:");
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            int cod = int.Parse(entrada[0]);
+            int qtde = int.Parse(entrada[1]);
+            double total;
+
+            switch (cod)
+            {
+                case 1:
+                    total = qtde * 4.00;
+                    break;
+                case 2:
+                    total = qtde * 4.50;
+                    break;
+                case 3:
+                    total = qtde * 5.00;
+                    break;
+                case 4:
+                    total = qtde * 2.00;
+                    break;
+                case 5:
+                    total = qtde * 1.50;
+                    break;
+                default:
+                    total = 0;
+                    break;
+            }
+
+            Console.WriteLine($"Total a pagar: R$ {total.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
